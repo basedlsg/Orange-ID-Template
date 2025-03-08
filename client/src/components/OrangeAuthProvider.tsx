9bee25b7dd3a2b4492379353a5746749da87ef00
josefandre <37456332-josefandre@users.noreply.replit.com>
2025-03-08 07:45:16 +0000

    Restored to 'cd8da34fb669b396238e5d88e8be81a332586979'
    Replit-Restored-To: cd8da34fb669b396238e5d88e8be81a332586979

--- client/src/components/OrangeAuthProvider.tsx
+++ client/src/components/OrangeAuthProvider.tsx
@@ -14,6 +14,7 @@ export function OrangeAuthProvider({
       authCallbackUrl={`${origin}/auth/callback`}
       tenantId="juice-town"
       walletConnectId={import.meta.env.VITE_WALLETCONNECT_PROJECT_ID}
+      disableWidget={true}
       widgetConfig={{
         enabled: false,
       }}
